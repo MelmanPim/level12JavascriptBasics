181a1b82c6df4ed4697dd53b30c0779c25567879
MelmanPim <59539663+MelmanPim@users.noreply.github.com>
2021-06-09 16:18:05 +0200

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,13 @@
+console.log("Hello Winc Academy")
+
+let name = 'Pim';
+let age = 23;
+let ageString = '23'
+
+let addition = 4 + 4;
+let additionString = '4' + '4';
+
+let subtraction = 6 + 4;
+let multiplication = 4 * 4;
+let division = 12 / 4;
+let modulus = 10 % 3;
